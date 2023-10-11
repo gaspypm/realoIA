7edb870bbcee4fe8c639a6ff9359384232efbc96
gaspypm <gaspypm@gmail.com>
2023-10-11 19:58:33 -0300

    add language dropdown

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,16 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+const language = 'en';
+
 function App() {
   const [score, setScore] = useState(0)
 
   return (
     <>
+    <div>
+      <h1>Puntaje: {score}</h1>
+    </div>
       <div>
         <a target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -18,13 +23,20 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
+        <button onClick={() => axios.get('https://thispersondoesnotexist.com/')}></button>
         <button onClick={() => setScore((score) => score + 1)}>
-          Score {score}
+          Jugar de nuevo
         </button>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <div className="language-selector">
+      <select value={language} onChange={(e) => handleChangeLanguage(e.target.value)}>
+        <option value="en">English</option>
+        <option value="es">Español</option>
+      </select>
+    </div>
     </>
   )
 }
