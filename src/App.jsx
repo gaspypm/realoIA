2544cb384eadeccac3b3fbff1427cbbd6720aaa6
gaspypm <gaspypm@gmail.com>
2023-10-11 19:29:50 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [score, setScore] = useState(0)
+
+  return (
+    <>
+      <div>
+        <a target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setScore((score) => score + 1)}>
+          Score {score}
+        </button>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
